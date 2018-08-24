333a52b9e976655ea83515baaa436dc8caaf07ba
Horace Williams <horace.d.williams@gmail.com>
2018-08-23 21:41:23 -0700

    Round robin queue test

--- src/geoq/par.rs
+++ src/geoq/par.rs
@@ -1,8 +1,9 @@
 extern crate deque;
 extern crate rand;
 
-use self::deque::{Stealer, Stolen};
+// use self::deque::{Stealer, Stolen};
 use std::thread::{self, JoinHandle};
+use std::sync::mpsc::{sync_channel, SyncSender, Receiver, RecvError};
 
 enum Output {
     Item((usize, f32)),
@@ -16,49 +17,77 @@ enum Input {
 
 fn it() -> Vec<(usize, f32)> {
     let num_workers = 4;
-    let (worker, stealer) = deque::new();
-    (1..20).for_each(|i| worker.push(Input::Item(i)));
-    (1..num_workers).for_each(|i| worker.push(Input::Done));
 
+    let mut input_channels: Vec<SyncSender<Input>> = vec![];
     let mut threads: Vec<JoinHandle<_>> = vec![];
-    let mut queues: Vec<Stealer<Output>> = vec![];
-    (1..num_workers).for_each(|_| {
-        let stealer = stealer.clone();
-        let (worker_writer, worker_output) = deque::new();
+    let mut output_channels: Vec<Receiver<Output>> = vec![];
+    (0..num_workers).for_each(|i| {
+        println!("Start worker {}", i);
+        let (input_sender, input_receiver) = sync_channel(50);
+        let (output_sender, output_receiver) = sync_channel(50);
+
         let t = thread::spawn(move|| {
             loop {
-                let work = stealer.steal();
+                let work = input_receiver.recv();
                 match work {
-                    Stolen::Empty => continue,
-                    Stolen::Abort => continue,
-                    Stolen::Data(Input::Item(i)) => {
-                        worker_writer.push(Output::Item((i, rand::random())));
+                    Err(RecvError) => continue,
+                    Ok(Input::Item(i)) => {
+                        output_sender.send(Output::Item((i, rand::random()))).unwrap();
                     }
-                    Stolen::Data(Input::Done) => {
-                        worker_writer.push(Output::Done);
+                    Ok(Input::Done) => {
+                        output_sender.send(Output::Done).unwrap();
                         break;
                     }
                 }
             }
         });
-        queues.push(worker_output);
+
+        input_channels.push(input_sender);
+        output_channels.push(output_receiver);
         threads.push(t);
     });
 
+    for i in 0..20 {
+        println!("enqueue to worker {}", i % num_workers);
+        input_channels[i % num_workers].send(Input::Item(i)).unwrap();
+    }
+    (0..num_workers).for_each(|i| input_channels[i].send(Input::Done).unwrap() );
+
     println!("{:?}", threads);
-    println!("{:?}", queues);
+    println!("{:?}", output_channels);
     // let last_received = 0;
-    while !queues.is_empty() {
-        for i in (0..queues.len()) {
-            let stolen = queues[i].steal();
-            match stolen {
-                Stolen::Empty => continue,
-                Stolen::Abort => continue,
-                Stolen::Data(Output::Item((i, f))) => {
+    // SPSC approach
+
+    // Workers 1,2,3
+    // SPSC 1 Reader ----> Worker 1
+    // SPSC 2 Reader ----> Worker 2
+    // SPSC 3 Reader ----> Worker 3
+
+    // SPSC 4 Worker 1 ----> Printer
+    // SPSC 5 Worker 2 ----> Printer
+    // SPSC 6 Worker 3 ----> Printer
+
+    // Reader
+    // Enqueue lines in order round-robin 1,2,3,1,2,3
+    // Worker
+    // Push to owned output queue as done
+    // Printer
+    // Round-robin blocking read from worker output queues
+
+    while !output_channels.is_empty() {
+        println!("Printer loop");
+        for i in 0..output_channels.len() {
+            println!("Printer check channel {}", i);
+            let output = output_channels[i].recv();
+            match output {
+                Err(RecvError) => continue,
+                Ok(Output::Item((i, f))) => {
                     println!("output received ({}, {})", i, f);
-                }
-                Stolen::Data(Output::Done) => {
-                    queues.remove(i);
+                },
+                Ok(Output::Done) => {
+                    println!("Received done from {}", i);
+                    output_channels.remove(i);
+                    println!("removed channel, num outputs is {}", output_channels.len());
                     break;
                 }
             }
