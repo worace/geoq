85f20476847f3729e5bfc83f5615f914c48e605f
Horace Williams <horace.d.williams@gmail.com>
2021-07-30 14:18:58 -0700

    researching more shp conversions

--- src/geoq/commands/shp.rs
+++ src/geoq/commands/shp.rs
@@ -1,9 +1,9 @@
-use crate::geoq::{error::Error};
+use crate::geoq::error::Error;
 use clap::ArgMatches;
-use shapefile;
 use dbase::{FieldValue, Record};
-use serde_json::{json, Map, Value, Number};
 use geojson;
+use serde_json::{Map, Number, Value};
+use shapefile;
 
 impl From<shapefile::Error> for Error {
     fn from(_err: shapefile::Error) -> Self {
@@ -24,7 +24,7 @@ fn record_to_json(record: Record) -> Result<serde_json::Map<String, Value>, Stri
             FieldValue::Character(Some(string)) => {
                 let json_str = serde_json::Value::String(string);
                 json.insert(name, json_str);
-            },
+            }
             FieldValue::Double(num) => {
                 let num = Number::from_f64(num).ok_or("Could not convert shp number to JSON")?;
                 let json_num = serde_json::Value::Number(num);
@@ -32,18 +32,52 @@ fn record_to_json(record: Record) -> Result<serde_json::Map<String, Value>, Stri
             }
             _ => (),
         }
-    };
+    }
     Ok(json)
 }
 
+// fn shp_to_gj_point(p: shapefile::Shape::PointZ)
+
 fn shp_to_gj_geom(geom: shapefile::Shape) -> Result<geojson::Geometry, String> {
     match geom {
-        shapefile::Shape::Point(point) => {
-            let gj_point = geojson::Value::Point(vec![point.x, point.y]);
+        shapefile::Shape::Point(g) => {
+            let gj_point = geojson::Value::Point(vec![g.x, g.y]);
             let gj_geom = geojson::Geometry::new(gj_point);
             Ok(gj_geom)
-        },
-        _ => Err("Invalid shape type".to_string())
+        }
+        shapefile::Shape::PointM(g) => {
+            let gj_point = geojson::Value::Point(vec![g.x, g.y, g.m]);
+            let gj_geom = geojson::Geometry::new(gj_point);
+            Ok(gj_geom)
+        }
+        shapefile::Shape::PointZ(g) => {
+            let gj_point = geojson::Value::Point(vec![g.x, g.y, g.z, g.m]);
+            let gj_geom = geojson::Geometry::new(gj_point);
+            Ok(gj_geom)
+        }
+        shapefile::Shape::Polyline(g) => {
+            // shapefile polyline becomes multilinestring
+            let parts = g.parts();
+            if (parts.len() == 0) {
+                Ok(geojson::Geometry::new(geojson::Value::LineString(vec![])))
+            } else if (parts.len() == 1) {
+                let part = g.part(0).or_else(vec![]);
+                // part.map(|point| )
+                Ok(geojson::Geometry::new(geojson::Value::LineString(vec![])))
+            } else {
+                for p in parts {
+                    print!("{:?}", p);
+                }
+                let gj_point = geojson::Value::Point(vec![0.0, 0.0]);
+                let gj_geom = geojson::Geometry::new(gj_point);
+                Ok(gj_geom)
+            }
+
+            // let gj_point = geojson::Value::Point(vec![g.x, g.y, g.z, g.m]);
+            // let gj_geom = geojson::Geometry::new(gj_point);
+            // Ok(gj_geom)
+        }
+        _ => Err("Invalid shape type".to_string()),
     }
 }
 
@@ -55,10 +89,9 @@ fn shp_to_geojson(geom: shapefile::Shape, record: Record) -> Result<geojson::Fea
         bbox: None,
         foreign_members: None,
         geometry: Some(gj_geom),
-        properties: Some(props)
+        properties: Some(props),
     })
 }
-
 
 pub fn run(m: &ArgMatches) -> Result<(), Error> {
     let path = m.value_of("path").unwrap();
