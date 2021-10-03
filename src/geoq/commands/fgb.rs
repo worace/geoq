6245bdfb463a56f1708f5e045c4cad1e986d94c9
Horace Williams <horace.d.williams@gmail.com>
2021-10-03 10:50:24 -0700

    WIP figuring out fgb writer structure

--- src/geoq/commands/fgb.rs
+++ src/geoq/commands/fgb.rs
@@ -1,20 +1,47 @@
-use crate::geoq::{error::Error, par, reader::Reader, entity::Entity};
+use crate::geoq::{entity::Entity, error::Error, reader::Reader};
 use clap::ArgMatches;
-use geojson::GeoJson;
-use flatbuffers::{FlatBufferBuilder, UOffsetT};
-use flatgeobuf::{Header, Feature};
+use flatbuffers::{FlatBufferBuilder, UOffsetT, WIPOffset};
+use flatgeobuf::{Feature, Header, HeaderBuilder};
 use std::io;
 
+// https://www.notion.so/worace/Flatgeobuf-4c2eb8ea1475419991863f36bd2fa355
 
 fn write_fgb_feature(bldr: &mut FlatBufferBuilder, entity: &Entity) -> UOffsetT {
-    flatgeobuf::GeometryOffset
-    let args = flatgeobuf::FeatureArgs{
+    // flatgeobuf::GeometryOffset
+    let args = flatgeobuf::FeatureArgs {
         columns: None,
         geometry: None,
-        properties: None
+        properties: None,
     };
     let offset = flatgeobuf::Feature::create(bldr, &args);
     offset.value()
+}
+
+// table Header {
+//   name: string;                 // Dataset name
+//   envelope: [double];           // Bounds
+//   geometry_type: GeometryType;  // Geometry type (should be set to Unknown if per feature geometry type)
+//   has_z: bool = false;           // Does geometry have Z dimension?
+//   has_m: bool = false;           // Does geometry have M dimension?
+//   has_t: bool = false;           // Does geometry have T dimension?
+//   has_tm: bool = false;          // Does geometry have TM dimension?
+//   columns: [Column];            // Attribute columns schema (can be omitted if per feature schema)
+//   features_count: ulong;        // Number of features in the dataset (0 = unknown)
+//   index_node_size: ushort = 16; // Index node size (0 = no index)
+//   crs: Crs;                     // Spatial Reference System
+//   title: string;                // Dataset title
+//   description: string;          // Dataset description (intended for free form long text)
+//   metadata: string;             // Dataset metadata (intended to be application specific and suggested to be structured fx. JSON)
+// }
+fn write_header<'a>(
+    bldr: &'a mut FlatBufferBuilder,
+    features: Vec<geojson::Feature>,
+) -> WIPOffset<Header<'a>> {
+    // https://github.com/flatgeobuf/flatgeobuf/blob/master/src/fbs/header.fbs
+    let name = bldr.create_string("Geoq-generated FGB");
+    let mut hb = HeaderBuilder::new(bldr);
+    hb.add_name(name);
+    hb.finish()
 }
 
 fn write() -> Result<(), Error> {
@@ -26,18 +53,22 @@ fn write() -> Result<(), Error> {
     let mut features: Vec<Feature> = Vec::new();
     let mut builder = FlatBufferBuilder::new();
 
+    let mut input_features: Vec<geojson::Feature> = Vec::new();
+
     let stdin = io::stdin();
     let mut stdin_reader = stdin.lock();
     let reader = Reader::new(&mut stdin_reader);
+
     for e_res in reader {
         match e_res {
             Err(e) => return Err(e),
             Ok(e) => {
-                write_fgb_feature(&mut builder, &e);
-            },
+                input_features.push(e.geojson_feature());
+            }
         }
     }
 
+    // write_fgb_feature(&mut builder, &e);
     Ok(())
 }
 
