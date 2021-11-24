1e07c617776b3831ffa43874a0d4050de81667ca
Horace Williams <horace.d.williams@gmail.com>
2021-11-23 20:24:35 -0800

    Switch off depreceated num_coords

--- src/geoq/coord_count.rs
+++ src/geoq/coord_count.rs
@@ -1,11 +1,12 @@
+use geo::algorithm::coords_iter::CoordsIter;
 use geo_types::{Geometry, Polygon};
 
 fn poly_coord_count(poly: &Polygon<f64>) -> usize {
-    poly.exterior().num_coords()
+    poly.exterior().coords_count()
         + poly
             .interiors()
             .iter()
-            .map(|ring| ring.num_coords())
+            .map(|ring| ring.coords_count())
             .sum::<usize>()
 }
 
@@ -15,10 +16,10 @@ pub fn coord_count(geom: &Geometry<f64>) -> usize {
         Geometry::Line(_) => 2,
         Geometry::Triangle(_) => 3,
         Geometry::Rect(_) => 4,
-        Geometry::LineString(ref g) => g.num_coords(),
+        Geometry::LineString(ref g) => g.coords_count(),
         Geometry::Polygon(ref g) => poly_coord_count(g),
         Geometry::MultiPoint(ref g) => g.0.len(),
-        Geometry::MultiLineString(ref g) => g.0.iter().map(|l| l.num_coords()).sum(),
+        Geometry::MultiLineString(ref g) => g.0.iter().map(|l| l.coords_count()).sum(),
         Geometry::MultiPolygon(ref g) => g.0.iter().map(|p| poly_coord_count(p)).sum(),
         Geometry::GeometryCollection(ref gc) => gc.0.iter().map(|g| coord_count(g)).sum(),
     }
